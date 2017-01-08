10814fb72f01a41bd9f9d3f01bff83319d39bf3c
iliyaST <iliamartinovstambolov@gmail.com>
2017-01-08 22:22:42 +0200

    Added 3 abstract classes Human,Ork and Undead and some Common folders for extensions and validations.

---
+++ DuelToTheDeath/Human.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public abstract class Human
+{
+    private string humanName;
+    private const int InitialHumanHitPoints = 700;
+    private const int InitialHumanDeffencePoints = 100;
+    private const int InitialHumanManaPoints = 200;
+    private const int InitialHumanManaRegeneration = 20;
+
+    public Human(string humanName)
+    {
+        this.humanName = humanName;
+        this.HitPoints = InitialHumanHitPoints;
+        this.DeffencePoints = InitialHumanDeffencePoints;
+        this.ManaPoints = InitialHumanManaPoints;
+        this.ManaPoints = InitialHumanManaRegeneration;
+    }
+
+    public string HumanName
+    {
+        get
+        {
+            return this.humanName;
+        }
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException();
+            }
+
+            if (value.Substring(0, 4) != "HuMn_")
+            {
+                throw new ArgumentException();
+            }
+
+            this.humanName = value;
+
+        }
+    }
+
+    public int HitPoints { get; set; }
+    public int DeffencePoints { get; set; }
+    public int ManaPoints { get; set; }
+    public int ManaRegeneration { get; set; }
+}
---
+++ DuelToTheDeath/Ork.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public abstract class Ork
+{
+    private string orkName;
+    private const int InitialOrkHitPoints = 1000;
+    private const int InitialOrkAttackPoints = 20;
+    private const int InitialOrkDeffencePoints = 200;
+    private const int InitialOrkHpRegeneration = 20;
+
+    public Ork(string orkName)
+    {
+        this.OrkName = orkName;
+        this.HitPoints = InitialOrkHitPoints;
+        this.AttackPoints = InitialOrkAttackPoints;
+        this.DeffencePoints = InitialOrkDeffencePoints;
+        this.HPRegeneration = InitialOrkHpRegeneration;
+    }
+
+    public string OrkName
+    {
+        get
+        {
+            return this.orkName;
+        }
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException();
+            }
+
+            if (value.Substring(0, 4) != "RAzN_")
+            {
+                throw new ArgumentException();
+            }
+
+            this.orkName = value;
+        }
+    }
+
+    public int HitPoints { get; set; }
+    public int DeffencePoints { get; set; }
+    public int HPRegeneration { get; set; }
+    public int AttackPoints { get; set; }
+}
